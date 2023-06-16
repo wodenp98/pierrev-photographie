71ff599ccfe9f1c23549fb5f8f30c0bfd6287267
Paul VIGNERON <paulvigneron4698@gmail.Com>
2023-06-16 13:14:48 +0200

    try some images for contact page

--- src/app/a-propos/page.tsx
+++ src/app/a-propos/page.tsx
@@ -1,3 +1,33 @@
-export default function Page() {
-  return <div>page</div>;
+import Image from "next/image";
+
+/* eslint-disable react/no-unescaped-entities */
+export default function About() {
+  return (
+    <main>
+      <ul className="flex ml-6">
+        <li className="text-gray-300">Accueil</li>
+        <li className="text-gray-300 mx-2">-</li>
+        <li>A propos</li>
+      </ul>
+      <h1 className="ml-6 mt-6 text-4xl">A propos</h1>
+      <section className="flex flex-col items-center ">
+        <p className="w-5/6">
+          Habitant la côte atlantique, j'aime mettre en valeur les paysages qui
+          m'entourent, mais aussi l'architecture et les habitants qui font vivre
+          cette belle région. Ainsi, je vous propose une invitation au voyage à
+          travers mes clichés. Mon travail est d'offrir à chacun la possibilité
+          de trouver une résonnance dans la photographie qu'il contemple. Je
+          serai ravi de discuter avec vous si l\ 'envie vous en dit, n'hésitez
+          donc pas à me contacter et à suivre mes aventures photographiques sur
+          mon compte Instagram ! ​ Bon visionnage ! Pierre V.
+        </p>
+        <Image
+          src="/photo-pierre.jpg"
+          alt="Pierre Vigneron"
+          width={325}
+          height={200}
+        />
+      </section>
+    </main>
+  );
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,67 @@
-export default function page() {
-  return <div>page</div>;
+/* eslint-disable react/no-unescaped-entities */
+import Image from "next/image";
+import { IoMdPaperPlane } from "react-icons/io";
+
+export default function Contact() {
+  return (
+    <main>
+      <ul className="flex ml-6">
+        <li className="text-gray-300">Accueil</li>
+        <li className="text-gray-300 mx-2">-</li>
+        <li>Contact</li>
+      </ul>
+      <h1 className="ml-6 mt-6 text-4xl">Contact</h1>
+      <section className="flex flex-col items-center">
+        <h2 className="w-4/5">
+          Pour toute question, n'hésitez pas à me contacter ! Je vous répondrai
+          dans les meilleurs délais. A bientôt !
+        </h2>
+        <div className="relative w-full">
+          <Image
+            src="/assets/mailfond.jpg"
+            alt="Votre image"
+            className="w-full h-auto"
+            width={325}
+            height={325}
+          />
+          <div className="absolute inset-0 bg-gray-400 bg-opacity-40 flex flex-col items-center justify-center">
+            <IoMdPaperPlane className="text-4xl text-gray-900" />
+            <p className="text-gray-900 text-lg font-bold">
+              pierre.v.photographie@gmail.com
+            </p>
+          </div>
+        </div>
+        <div className="relative w-full">
+          <Image
+            src="/assets/mailfond.jpg"
+            alt="Votre image"
+            className="w-full h-auto"
+            width={325}
+            height={325}
+          />
+          <div className="absolute inset-0 bg-gray-400 bg-opacity-40 flex flex-col items-center justify-center">
+            <IoMdPaperPlane className="text-4xl text-gray-900" />
+            <p className="text-gray-900 text-lg font-bold">
+              pierre.v.photographie@gmail.com
+            </p>
+          </div>
+        </div>
+        <div className="relative w-full">
+          <Image
+            src="/assets/mailfond.jpg"
+            alt="Votre image"
+            className="w-full h-auto"
+            width={325}
+            height={325}
+          />
+          <div className="absolute inset-0 bg-gray-400 bg-opacity-40 flex flex-col items-center justify-center">
+            <IoMdPaperPlane className="text-4xl text-gray-900" />
+            <p className="text-gray-900 text-lg font-bold">
+              pierre.v.photographie@gmail.com
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
